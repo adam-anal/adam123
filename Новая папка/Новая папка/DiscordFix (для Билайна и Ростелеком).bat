1a7d2c2b0e7cfcb351aaf326aa7596dd9f6de97f
adam-anal <fifainglish@gmail.com>
2024-12-18 15:36:39 +0700

    Add files via upload

---
+++ Новая папка/Новая папка/DiscordFix (для Билайна и Ростелеком).bat
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+cd /d "%~dp0"
+set BIN=%~dp0bin\
+
+set LIST_TITLE=ZAPRET: Discord Fix Beeline-Rostelekom
+set LIST_PATH=%~dp0lists\list-discord.txt
+
+start "%LIST_TITLE%" /min "%BIN%winws.exe" --wf-udp=50000-65535 ^
+--filter-udp=50000-65535 --hostlist="%LIST_PATH%" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,split2
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,split2 --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,disorder2
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,disorder2 --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata --wssize 1:6
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin --wssize 1:6
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,split2 --wssize 1:6
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,split2 --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin --wssize 1:6
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,disorder2 --wssize 1:6
+--wf-l3=ipv4 --wf-tcp=443 --dpi-desync=syndata,disorder2 --dpi-desync-fake-syndata=/cygdrive/c/zapret-win-bundle-master/blockcheck/zapret/files/fake/tls_clienthello_iana_org.bin --wssize 1:6
